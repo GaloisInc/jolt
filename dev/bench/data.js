0015349cc13bb18e025ccc715b6bdaedd3a2eb62
github-action-benchmark <github@users.noreply.github.com>
2024-12-31 22:13:51 +0000

    add CN Benchmarks (customSmallerIsBetter) benchmark result for 7ff5096860db82fb9fdd4fb1ec1159a61c2e1430

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1735683231242,
+  "repoUrl": "https://github.com/GaloisInc/jolt",
+  "entries": {
+    "CN Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "aditya.zutshi@galois.com",
+            "name": "Aditya Zutshi",
+            "username": "zutshi"
+          },
+          "committer": {
+            "email": "aditya.zutshi@galois.com",
+            "name": "Aditya Zutshi",
+            "username": "zutshi"
+          },
+          "distinct": true,
+          "id": "7ff5096860db82fb9fdd4fb1ec1159a61c2e1430",
+          "message": "added cahcing",
+          "timestamp": "2024-12-31T13:49:49-08:00",
+          "tree_id": "90ddd16790d5d71dfa93a19899685c92201693a6",
+          "url": "https://github.com/GaloisInc/jolt/commit/7ff5096860db82fb9fdd4fb1ec1159a61c2e1430"
+        },
+        "date": 1735683228957,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "CPU Load",
+            "value": 50,
+            "unit": "Percent",
+            "range": "5%",
+            "extra": "My Optional Information for the tooltip"
+          },
+          {
+            "name": "Peak Memory Used",
+            "value": 100,
+            "unit": "Megabytes"
+          }
+        ]
+      }
+    ]
+  }
+}
