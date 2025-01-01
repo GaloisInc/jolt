b48f500dd1dafea081448a6515c1238ce8de8daa
github-action-benchmark <github@users.noreply.github.com>
2025-01-01 02:02:28 +0000

    add Benchmarks (customSmallerIsBetter) benchmark result for 5aa9aeef28b89ee9bc3d52ee05577517c63d2180

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1735696948642,
+  "repoUrl": "https://github.com/GaloisInc/jolt",
+  "entries": {
+    "Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "aditya.zutshi@galois.com",
+            "name": "Aditya Zutshi",
+            "username": "zutshi"
+          },
+          "committer": {
+            "email": "aditya.zutshi@galois.com",
+            "name": "Aditya Zutshi",
+            "username": "zutshi"
+          },
+          "distinct": true,
+          "id": "5aa9aeef28b89ee9bc3d52ee05577517c63d2180",
+          "message": "minor: changed name",
+          "timestamp": "2024-12-31T17:56:05-08:00",
+          "tree_id": "58ff21348cacddb9fcc700dc3d53b8cfd39f3b74",
+          "url": "https://github.com/GaloisInc/jolt/commit/5aa9aeef28b89ee9bc3d52ee05577517c63d2180"
+        },
+        "date": 1735696947355,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "fibonacci",
+            "value": 188.91,
+            "unit": "s",
+            "extra": ""
+          },
+          {
+            "name": "muldiv",
+            "value": 136.75,
+            "unit": "s",
+            "extra": ""
+          }
+        ]
+      }
+    ]
+  }
+}
