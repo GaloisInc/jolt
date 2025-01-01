7b76f0807e9c347cd7028b295dbc8c00a54ec749
github-action-benchmark <github@users.noreply.github.com>
2025-01-01 01:51:54 +0000

    add Benchmarks (customSmallerIsBetter) benchmark result for dbd5aaf4439a80ea16381614857e896f809c2b86

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1735683231242,
+  "lastUpdate": 1735696314379,
   "repoUrl": "https://github.com/GaloisInc/jolt",
   "entries": {
     "CN Benchmarks": [
@@ -36,6 +36,44 @@ window.BENCHMARK_DATA = {
             "name": "Peak Memory Used",
             "value": 100,
             "unit": "Megabytes"
+          }
+        ]
+      }
+    ],
+    "Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "aditya.zutshi@galois.com",
+            "name": "Aditya Zutshi",
+            "username": "zutshi"
+          },
+          "committer": {
+            "email": "aditya.zutshi@galois.com",
+            "name": "Aditya Zutshi",
+            "username": "zutshi"
+          },
+          "distinct": true,
+          "id": "dbd5aaf4439a80ea16381614857e896f809c2b86",
+          "message": "added result generation",
+          "timestamp": "2024-12-31T17:45:23-08:00",
+          "tree_id": "73a7093431ee2007586b880cabf05cda15149608",
+          "url": "https://github.com/GaloisInc/jolt/commit/dbd5aaf4439a80ea16381614857e896f809c2b86"
+        },
+        "date": 1735696312454,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "fibonacci",
+            "value": 196.8,
+            "unit": "s",
+            "extra": ""
+          },
+          {
+            "name": "muldiv",
+            "value": 136.14,
+            "unit": "s",
+            "extra": ""
           }
         ]
       }
